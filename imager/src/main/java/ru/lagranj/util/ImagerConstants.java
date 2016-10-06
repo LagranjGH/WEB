5f567ade1250ec756a94e3a5a7a172271005a18e
LagranjGH <palozz@yandex.ru>
2016-10-06 11:23:21 +0300

    mini refactoring

--- imager/src/main/java/ru/lagranj/util/ImagerConstants.java
+++ imager/src/main/java/ru/lagranj/util/ImagerConstants.java
@@ -1,15 +1,15 @@
 package ru.lagranj.util;
 
-public interface ImagerConstants {
-	String ROOT_PATH = "app_path";
-	String IMG_PARAM_NAME = "img_url";
-	String FIRST_DIRECTORY_NAME = "1";
-	String FILE_SEPARATOR = System.getProperty("file.separator");
-	String STRING_DOT = ".";
-	String STRING_UNDERSCORE = "_";
-	int RELOAD_PROPERTIES_PERIOD = 60;
-	int DEFAULT_SUBDIRECTORY_SIZE = 32;
-	boolean DEFAULT_AUTO_CLOSE = false;
-	int DEFAULT_AUTO_CLOSE_PERIOD = 2500;
-	long MULT_CONVERT_BYTES_TO_MEGABYTES = 1024L * 1024L;
+public final class ImagerConstants {
+	public static final String ROOT_PATH = "app_path";
+	public static final String IMG_PARAM_NAME = "img_url";
+	public static final String FIRST_DIRECTORY_NAME = "1";
+	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	public static final String STRING_DOT = ".";
+	public static final String STRING_UNDERSCORE = "_";
+	public static final int RELOAD_PROPERTIES_PERIOD = 60;
+	public static final int DEFAULT_SUBDIRECTORY_SIZE = 32;
+	public static final boolean DEFAULT_AUTO_CLOSE = false;
+	public static final int DEFAULT_AUTO_CLOSE_PERIOD = 2500;
+	public static final long MULT_CONVERT_BYTES_TO_MEGABYTES = 1024L * 1024L;
 }
